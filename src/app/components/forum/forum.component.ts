312277fc5ca8d7abfa4bbeadc740add947fcad60
Marko Šeparović <mseparov@unipu.hr>
2022-05-26 19:48:02 +0200

    Marko Commit (Megi user assist)

--- src/app/components/forum/forum.component.ts
+++ src/app/components/forum/forum.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/compat';
 
 @Component({
   selector: 'app-forum',
@@ -9,7 +10,12 @@ export class ForumComponent implements OnInit {
 
   constructor() { }
 
+  user:any = [{}]
+
   ngOnInit(): void {
+    this.user = localStorage.getItem('user')
+    this.user = JSON.parse(this.user)
+    console.log("user: ", this.user.email)
   }
 
 }
