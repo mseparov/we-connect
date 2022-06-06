b38effe838f5167e2c360bec1ccbff5b234636a5
Marko Šeparović <mseparov@unipu.hr>
2022-06-07 01:16:54 +0200

    Marko Commit Netlify Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCkNf-PQwwsQ8nEHv5Ubhz6BJuHhvt1DVg",
+    authDomain: "we-connect-980df.firebaseapp.com",
+    projectId: "we-connect-980df",
+    storageBucket: "we-connect-980df.appspot.com",
+    messagingSenderId: "555353038171",
+    appId: "1:555353038171:web:07c915e276709c2bff0e7e"
+  }
+
 };
